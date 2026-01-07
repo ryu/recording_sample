e01aa182f02f8045ae692f6c971a34727abd2fd2
Ryu Yamamoto <ryu.yamamoto@icloud.com>
2026-01-07 15:03:06 +0900

    Fix lint errors.

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -51,6 +51,6 @@ class RecordingsController < ApplicationController
   private
 
     def document_params
-      params.expect(document: [:title, :body])
+      params.expect(document: [ :title, :body ])
     end
 end
